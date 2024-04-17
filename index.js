5ae2b7efd36a5f85da0761e2b553188b5beb12dc
ylongtay <145927097+ylongtay@users.noreply.github.com>
2024-04-17 21:11:55 +0800

    Added new introduce endpoint

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const print = require("./controller");
 
 app.get('/', print);
 // app.get("/hello")
+
+app.get("/introduce", (req, res) => {
+    res.send("Hello, I am YONG LONG");
+});
+
 app.listen(process.env.PORT, ()=>{
     console.log(`Listening to port ${process.env.PORT}`)
 })
