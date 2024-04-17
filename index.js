f1d27b161ee683ed235b7102ac5c530e309f8292
ylongtay <145927097+ylongtay@users.noreply.github.com>
2024-04-17 21:18:39 +0800

    Added new home endpoint

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get("/introduce", (req, res) => {
     res.send("Hello, I am YONG LONG");
 });
 
+app.get("/home", (req, res) => {
+  res.send("This is the home page");
+});
+
 app.listen(process.env.PORT, ()=>{
     console.log(`Listening to port ${process.env.PORT}`)
 })
